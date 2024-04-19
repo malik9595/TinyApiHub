469c1a637313a54dfd1aea5537a6130a6ca35560
malik9595 <mailerfakemail@gmail.com>
2024-04-19 20:19:11 +0530

    updated

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname,'public')))
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
 
 mongoose
-	.connect(`mongodb+srv://malik:malik@myclustername.ym6pqyq.mongodb.net/`)
+	.connect(`${MongoDb_Connection_Id}`)
 	.then(() => console.log('DB Connected'))
 	.catch((err) => console.log(`something went wrong: ${err}`));
 
--- test.js
+++
@@ -1,3 +0,0 @@
-let x=12, y =7
-const num = x || y || "yes"
-console.log(num)
